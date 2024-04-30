70f37e374a55c28a3ac24e554e19c6fc785ad78c
Frederick <antoniobalderramamendoza@gmail.com>
2024-04-30 09:01:52 -0400

    P3-001 added Library Class 'Business Logic', Model Patient and Manager 'Patient Manager', also CreatePatient function and GetRandomBloodType and function

---
+++ BusinessLogic/Managers/PatientManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UPB.BusinessLogic.Models;
+
+namespace UPB.BusinessLogic.Managers
+{
+    internal class PatientManager
+    {
+        public PatientManager() { }
+
+        public Patient CreatePatient(string name, string lastname, int CI)
+        {
+            Patient createdPatient = new Patient()
+            {
+                Name = name,
+                LastName = lastname,
+                CI = CI,
+                BloodType = GetRandomBloodType()
+            };
+
+            return createdPatient;
+        }
+
+        public string GetRandomBloodType()
+        {
+            Random rand = new Random();
+            int random_value = rand.Next(0, 81);
+
+            if(random_value < 10)
+            {
+                return "A+";
+            }
+
+            if(random_value < 20)
+            {
+                return "A-";
+            }
+
+            if (random_value < 30)
+            {
+                return "B+";
+            }
+
+            if (random_value < 40)
+            {
+                return "B-";
+            }
+
+            if (random_value < 50)
+            {
+                return "AB+";
+            }
+
+            if (random_value < 60)
+            {
+                return "AB-";
+            }
+
+            if (random_value < 70)
+            {
+                return "O+";
+            }
+
+            return "O-";
+        }
+    }
+}
